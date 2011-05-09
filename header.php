b2429c66b491fc19c8a9e923ab6d4f0fb9d55dda
Mike Wilson <mwilson@zuul.(none)>
2011-05-09 15:37:57 -0400

    Upgraded to HTML5 Removed search widget from default install sidebar

--- header.php
+++ header.php
@@ -4,14 +4,15 @@
  * @subpackage Default_Theme
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
-<head profile="http://gmpg.org/xfn/11">
+<head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
+<link rel="profile" href="http://gmpg.org/xfn/11" />
 <?php if (is_mobile()): ?>
     <style type="text/css">
     <?php include( 'style_mobile.css' ); ?>
